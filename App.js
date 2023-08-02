70676d758eb7ebca208bf626183966d2566c98bc
tashhirul990 <tashhirul990@gmail.com>
2023-08-02 15:47:30 +0530

    Basic concepts of React

---
+++ App.js
@@ -0,0 +1,14 @@
+const perent = React.createElement("div", { id: "perent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(perent);
